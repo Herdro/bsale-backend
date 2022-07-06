c2973aede43f7d31e536e979a3cc34bd764f7866
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-07-06 13:53:44 -0400

    add driver MySQL (sequelize) config

---
+++ src/drivers/MySQL/sequelize.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const { config } = require('../../../config/config');
+const setupModels = require('../../domains/db/model');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPass);
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}/${config.dbName}`;
+
+const sequelize = new Sequelize(URI, {
+  dialect: 'mysql',
+  logging: true,
+});
+
+setupModels(sequelize);
+
+module.exports = sequelize;
